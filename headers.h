6a47ecbb695f427c86876b8b042e8e7ba09a3ca5
Raja Shekar <raja.sj@pthpartnertech.com>
2018-07-27 12:20:38 +0530

    added headers file containig headers

---
+++ headers.h
@@ -0,0 +1,19 @@
+#ifndef __HEADERS_H_
+#define __HEADERS_H_
+
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/types.h>
+#include<linux/kdev_t.h>
+#include<linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/moduleparam.h>
+#include <linux/errno.h>        /* error codes */
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>        /* copy_*_user */
+#include <linux/uaccess.h>
+
+#endif /*End of Headers.h*/
+
+
